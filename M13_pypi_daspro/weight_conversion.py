95f2aa9e6af3bf0d05aec2b7bb7a2b89e143b3b5
afatur_dk <affadhika287@gmail.com>
2023-12-13 08:21:45 +0700

    add pypi

--- M13_pypi_daspro/weight_conversion.py
+++ M13_pypi_daspro/weight_conversion.py
@@ -4,7 +4,7 @@
 
 def weight_conversion():
     berat = int(input("Masukkan berat anda > "))
-    satuan = int(input("Dalam satuan apa berat yang anda masukkan > (K untuk KG, L untuk LBS) > "))
+    satuan = input("Dalam satuan apa berat yang anda masukkan > (K untuk KG, L untuk LBS) > ")
     
     if satuan.lower() == 'l':
         print("Berat anda dikonversi menjadi kilogram adalah {round(berat * 0.453592)} kg")
